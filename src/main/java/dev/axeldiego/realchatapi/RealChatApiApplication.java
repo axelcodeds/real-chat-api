b8ba02a6d05d0eb241b36272ec4c85bd6f255a63
Axel <axel@example.com>
2026-05-03 22:56:31 -0600

    first commit

---
+++ src/main/java/dev/axeldiego/realchatapi/RealChatApiApplication.java
@@ -0,0 +1,13 @@
+package dev.axeldiego.realchatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealChatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/axeldiego/realchatapi/RealChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.axeldiego.realchatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealChatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
